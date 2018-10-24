2344aa0f49ff69f96c6790eba688c77a41a6fdef
zackers14 <zachary.mericle@gmail.com>
2018-10-24 15:01:30 -0400

    Additional cleanup of unnecessary components of project 3 code

--- parallel_merge_sort.c
+++ parallel_merge_sort.c
@@ -28,8 +28,6 @@ typedef struct
   int integer_array[];
 } shared_mem;
 
-
-
 pthread_t       tids[];
 pthread_attr_t  attr[1]; //Attribute pointer array
 shared_mem *sh_mem;
@@ -54,7 +52,7 @@ int main(){
   }
 
   //Connect to shared memory section
-  if ((circular_buffer = (shared_mem *) shmat(shmid, shmadd, 0)) == (shared_mem *)-1){
+  if ((sh_mem = (shared_mem *) shmat(shmid, shmadd, 0)) == (shared_mem *)-1){
     perror("shmat");
     exit(0);
   }
@@ -74,11 +72,9 @@ int main(){
 
   //Terminate semaphores
   sem_destroy(&mutex);
-  sem_destroy(&full);
-  sem_destroy(&empty);
 
   //Detach and remove shared memory
-  if (shmdt(circular_buffer) == -1){
+  if (shmdt(sh_mem) == -1){
     perror("shmdt");
     exit(-1);
   }
@@ -88,71 +84,4 @@ int main(){
   pthread_exit(NULL);
 }
 
-// This function is for the consumer thread, it reads from a shared memory
-// character buffer
-void* consumer(void* arg){
-  char read_char;
-  int read_index = 0;
-  int end_of_file = 0;
-
-  //Loops while the end of file has not been found
-  while (!end_of_file){
-  //Delay of 1 second
-  sleep(1);
-
-  sem_wait(&full);
-
-  sem_wait(&mutex);
-
-  // Critical section
-  read_char = circular_buffer->buffer[read_index%15];
-
-  sem_post(&mutex);
-
-  // * is the character the producer will put in the buffer once it has reached
-  // EOF, will print character if * is not found yet
-  if (read_char == '*') end_of_file = 1;
-  else printf("%c", read_char);
-
-  fflush(stdout);
-  read_index++;
-
-  sem_post(&empty);
-
-  }
-}
-
-void* producer(void* arg){
-  char new_char;
-  int write_index = 0;
-  FILE * fp;
-  fp = fopen("mytest.dat", "r");
-
-  // Loops while the file has remaining characters
-  while(fscanf(fp, "%c", &new_char) != EOF){
-
-  sem_wait(&empty);
-
-  sem_wait(&mutex);
-
-  //critical section
-  circular_buffer->buffer[write_index%15] = new_char;
-
-  sem_post(&mutex);
-
-  write_index++;
-
-  sem_post(&full);
-
-  }
-  // Once end of file has been reached, place * in buffer
-  sem_wait(&empty);
-  sem_wait(&mutex);
-  circular_buffer->buffer[write_index%15] = '*';
-  sem_post(&mutex);
-  sem_post(&full);
-
-  //Close file
-  close(fp);
-
-}*/
+*/
