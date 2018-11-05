2df91660bc03ca0f01b866de2aab10d796851963
Zachary Mericle <zachary.mericle@gmail.com>
2018-11-05 13:37:56 -0500

    Add files via upload

--- parallel_merge_sort.c
+++ parallel_merge_sort.c
@@ -28,7 +28,6 @@ typedef struct
   int integer_array[];
 } shared_mem;
 
-pthread_t       tids[];
 pthread_attr_t  attr[1]; //Attribute pointer array
 shared_mem *sh_mem;
 
@@ -169,18 +168,18 @@ int main()
 }*/
 
 
-/*
-int main(){
+
+int main(int argc, char *argv[]){
   //Initialize necessary variables for shared memory
   int shmid;
+  int array_size = atoi(argv[1]);
   char * shmadd;
+  pthread_t tids[ceiling(array_size*(log10(array_size)/log10(2)))];
+
   shmadd = (char *) 0;
-  //Initialize the three semaphores
-  sem_init(&mutex, 0, 1);
-  sem_init(&full, 0, 0);
-  sem_init(&empty, 0, 15);
+  
   //Create a shared memory section
-  if ((shmid = shmget(SHMKEY, 15*sizeof(char), IPC_CREAT | 0666)) < 0){
+  if ((shmid = shmget(SHMKEY, array_size*sizeof(int), IPC_CREAT | 0666)) < 0){
     perror("shmget");
     exit(1);
   }
@@ -193,14 +192,11 @@ int main(){
   pthread_attr_init(&attr[0]);
   pthread_attr_setscope(&attr[0], PTHREAD_SCOPE_SYSTEM);
   //Schedule thread independently END
-  //Create threads
-  pthread_create(&tid1[0], &attr[0], producer, NULL);
-  pthread_create(&tid2[0], &attr[0], consumer, NULL);
-  //Wait for threads to finish
-  pthread_join(tid1[0], NULL);
-  pthread_join(tid2[0], NULL);
-  //Terminate semaphores
-  sem_destroy(&mutex);
+
+  pthread_create(&tids[0], &attr[0], mergeSort(0, array_size-1, 0), NULL);
+
+  pthread_join(tids[0], NULL);
+
   //Detach and remove shared memory
   if (shmdt(sh_mem) == -1){
     perror("shmdt");
@@ -210,4 +206,3 @@ int main(){
 //Terminate threads
   pthread_exit(NULL);
 }
-*/
